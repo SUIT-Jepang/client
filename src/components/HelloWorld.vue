2306f7bc9250af0fe224ca17d2f4d93ec4dfb26b
romizaki <rzferdiyanto@gmail.com>
2021-01-21 22:19:24 +0700

    create vue

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="container col-6">
+    <h1>{{ msg }}</h1>
+    <div class="card" style="width: 18rem;">
+      <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <a href="#" class="card-link">Card link</a>
+          <a href="#" class="card-link">Another link</a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld'
+}
+</script>
+
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/views/Login.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="container justify-content-center mt-5">
+        <div class="row justify-content-center">
+            <div class="col-5 mt-1 img">
+              <img src="https://media1.tenor.com/images/b2a284afcda426722f679d3298566ced/tenor.gif" alt="" srcset="">
+            </div>
+            <div class="col-5 mt-1 login">
+                <div class="shadow-md p-5 mb-5 bg-black rounded">
+                    <h5 class="mb-4 text-center"><strong>BATU GUNTING KERTAS</strong></h5>
+                    <form @submit.prevent="login">
+                    <div class="form-group">
+                        <input type="text" class="form-control rounded-pill" v-model="name" id="login-name" placeholder="Your Name">
+                    </div>
+                    <button type="submit" class="btn btn-primary btn-block mt-5 rounded-pill">MASUK</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  data () {
+    return {
+      name: ''
+    }
+  },
+  methods: {
+    login () {
+      localStorage.setItem('name', this.name)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
