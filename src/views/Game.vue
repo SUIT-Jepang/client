68b6ceb6164b6539f60649920261d917de359775
romizaki <rzferdiyanto@gmail.com>
2021-01-22 08:29:24 +0700

    client adu done

--- src/views/Game.vue
+++ src/views/Game.vue
@@ -2,14 +2,29 @@
     <div class="" id="layout">
         <div class="container my-5">
             <h1>Welcome to Gunting Batu Kertas, {{user}}</h1>
-            <h4>{{userChoose}}</h4>
+            <h4>{{ winner }}</h4>
         </div>
         <div class="container d-flex justify-content-around my-5">
-            <button @click="choose('batu')" class="btn"><img src="../assets/rock.png" width="100"/> </button>
-            <button @click="choose('gunting')" class="btn"><img src="../assets/scissor.png" width="100"/> </button>
-            <button @click="choose('kertas')" class="btn"><img src="../assets/paper.png" width="100"/> </button>
+            <div>
+                <h1> {{ totalCounter.batu }} </h1>
+                <button @click="choose('batu')" class="btn"> 
+                    <img src="../assets/rock.png" width="100"/> 
+                </button>
+            </div>
+            <div>
+                <h1>{{ totalCounter.gunting }}</h1>
+                <button @click="choose('gunting')" class="btn">
+                    <img src="../assets/scissor.png" width="100"/> 
+                </button>
+            </div>
+            <div>
+                <h1> {{ totalCounter.kertas }} </h1>
+                <button @click="choose('kertas')" class="btn">
+                    <img src="../assets/paper.png" width="100"/>
+                </button>
+            </div>
         </div>
-        <button v-if="userChoose.length > 0" @click="submit" class="btn btn-primary">Adu !</button>
+        <button @click="reset" class="btn btn-danger">Mulai dari 0 !</button>
     </div>
 </template>
 
@@ -20,7 +35,8 @@ export default {
             userChoose : '',
             user: localStorage.getItem('name'),
             games: [],
-            totalCounter: { gunting: 10, kertas: 20, batu: 5 }
+            totalCounter: { gunting: 0, kertas: 0, batu: 0 },
+            winner: ''
         }
     },
     methods: {
@@ -32,15 +48,33 @@ export default {
         } else if (choose == 'kertas') {
             this.totalCounter.kertas++
         }
-        console.log(totalCounter)
+        this.currentWinner()
+        console.log(this.totalCounter)
         this.userChoose = choose
       },
-      submit(){
-        this.$socket.emit
-        this.games.push({ player: this.user, weapon:this.userChoose })
-        this.userChoose = ''
-        console.log('>>> submit',this.userChoose)
+      reset () {
+        this.totalCounter = { gunting: 0, kertas: 0, batu: 0 }
+        this.currentWinner()
+      },
+      currentWinner () {
+          let { batu, gunting, kertas } = this.totalCounter
+          if (batu > kertas && batu > kertas) {
+              this.winner = 'Batu menang'
+          } else if (kertas > batu && kertas > gunting) {
+              this.winner = 'Kertas menang'
+          } else if (gunting > batu && gunting > kertas) {
+              this.winner = 'Gunting menang'
+          } else {
+              if (!batu && !kertas && !gunting) {
+                this.winner = 'Main dulu bro'  
+              } else {
+                this.winner = 'Main lagi, seri bro'
+              }
+          }
       }
+    },
+    created() {
+        this.currentWinner()
     },
     socket: {
         init (payload) {
